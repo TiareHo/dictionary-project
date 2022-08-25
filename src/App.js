c5d3ff3d8c3f6900dc12e70fc20c7a6f1fac32b4
TiareHo <tiarehoegerman@gmail.com>
2022-08-25 15:40:15 -0700

    Installed Bootstrap CSS

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-Hello
+<button className="btn btn-outline-success"> Hello</button>Hello
       </header>
     </div>
   );
