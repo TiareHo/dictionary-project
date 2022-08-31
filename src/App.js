131214dbf3c97afb8b991d399e0b784c42325d01
TiareHo <tiarehoegerman@gmail.com>
2022-08-30 18:11:14 -0700

    Need to fade background image bottom somehow

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
       <header className="App-header">
       </header>
       <Dictionary />
-      <footer>This is a working project coded by <a href="https://www.linkedin.com/in/tiare-elise/" rel="noopener noreferrer" alt="Tiare">Tiare</a>. It is open-sourced via <a href="https://github.com/TiareHo/dictionary-project" rel="noopener noreferrer" alt="Tiare's GitHub">GitHub</a> and hosted on <a href="https://lambent-gecko-9837c1.netlify.app/" rel="noopener noreferrer" alt="Tiare's Netfily">Netlify</a>. Enjoy!</footer>
       </div>
+      <footer>This is a working project coded by <a href="https://www.linkedin.com/in/tiare-elise/" rel="noopener noreferrer" alt="Tiare">Tiare</a>. It is open-sourced via <a href="https://github.com/TiareHo/dictionary-project" rel="noopener noreferrer" alt="Tiare's GitHub">GitHub</a> and hosted on <a href="https://lambent-gecko-9837c1.netlify.app/" rel="noopener noreferrer" alt="Tiare's Netfily">Netlify</a>. Enjoy!</footer>
     </div>
   );
 }
